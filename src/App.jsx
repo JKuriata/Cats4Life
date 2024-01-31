454ff1f46b0f678eea0959e639aaca63d9937fe6
JKuriata <JK271096@outlook.com>
2024-01-31 12:40:43 +0000

    Added initial layout with cat images and randomised names/breeds/prices

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import { faker } from '@faker-js/faker';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [error, setErrorMsg] = useState("");
+  const [catData, setCatData] = useState([]);
+  const [basketItems, setBasketItems] = useState([]);
+  
+  const getAllCats = async () => {
 
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+
+      if (!response.ok) {
+        throw new Error("Something's not right...");
+      }
+      
+      const data = await response.json();
+      setCatData(data);
+      
+    } catch (error) {
+      console.log(error);
+      setErrorMsg(error.message);
+    }
+  };
+
+useEffect(() => {
+  getAllCats();
+}, []);
+
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="navBar">
+        <h1>Cats4Life</h1>
+        <p>🛒</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="catContainer">
+        {catData.map((cat, index) => {
+          return (
+            <div className="catCard" key={index}>
+              <div className='imgContainer'>
+                <img src={cat.url}/>
+              </div>
+              <div className='catCardText'>
+                <h2>{faker.person.firstName()}</h2>
+                <p>{faker.animal.cat()}</p>
+                <h3>£{faker.commerce.price()}</h3>
+              </div>
+            </div>
+          )
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="basket">
+      </div>
     </>
   )
 }
---
+++ src/assets/components/CatCard.jsx
@@ -0,0 +1,16 @@
+/*import { faker } from '@faker-js/faker';
+
+const CatCard = (props) => {
+    return (
+        <div className="catCard" key={index}>
+            <div className='imgContainer'>
+            <img src={cat.url}/>
+          </div>
+          <div className='catCardText'>
+            <h2>{faker.person.firstName()}</h2>
+            <p>{faker.animal.cat()}</p>
+            <h3>£{faker.commerce.price()}</h3>
+          </div>
+        </div>
+    )
+}*/
