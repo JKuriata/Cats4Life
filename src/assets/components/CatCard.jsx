be3868732259f4a8bdbccdd87a3a3522426dc931
JKuriata <JK271096@outlook.com>
2024-02-02 11:49:26 +0000

    Cat can now only be added to basket ONCE

--- src/assets/components/CatCard.jsx
+++ src/assets/components/CatCard.jsx
@@ -1,21 +1,34 @@
-const CatCard = (props) => {
-  let catID = `cat${(props.catInfo)._id}`;
-    return (
-        <div className="catCard" id={catID}>
-            <div className='imgContainer'>
-            <img src={(props.catInfo).image}/>
-          </div>
-          <div className='catCardText'>
-            <h2>{(props.catInfo).name}</h2>
-            <p>{(props.catInfo).breed}</p>
-            <div className="costBuy">
-            <p className="costText">£{(props.catInfo).cost}</p>
-            <button onClick={props.basketAdd}>Add to Basket</button>
-            </div>
-          </div>
+import React, { useState } from 'react';
 
+const CatCard = (props) => {
+  const [addedToBasket, setAddedToBasket] = useState(false);
+
+  const handleAddToBasket = () => {
+    if (!addedToBasket) { // Check if the cat has not been added to the basket
+      props.basketAdd(); // Add the cat to the basket
+      setAddedToBasket(true); // Update state to indicate that the cat has been added
+    }
+  };
+
+  let catID = `cat${props.catInfo._id}`;
+  
+  return (
+    <div className="catCard" id={catID}>
+      <div className='imgContainer'>
+        <img src={props.catInfo.image} alt={props.catInfo.name}/>
+      </div>
+      <div className='catCardText'>
+        <h2>{props.catInfo.name}</h2>
+        <p>{props.catInfo.breed}</p>
+        <div className="costBuy">
+          <p className="costText">£{props.catInfo.cost}</p>
+          <button onClick={handleAddToBasket} disabled={addedToBasket}>
+            {addedToBasket ? 'Added to Basket' : 'Add to Basket'}
+          </button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default CatCard;
